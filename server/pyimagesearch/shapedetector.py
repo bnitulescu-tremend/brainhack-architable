da755581774fa5a0ff60d85bcbfd3ba135f50f46
Bogdan Nitulescu <bogdan.nitulescu@tremend.com>
2018-12-07 21:52:41 +0200

    BANZAIgit add server.py pyimagesearch/shapedetector.py

--- server/pyimagesearch/shapedetector.py
+++ server/pyimagesearch/shapedetector.py
@@ -1,10 +1,69 @@
 # import the necessary packages
 import cv2
 
-class ShapeDetector:
-	def __init__(self):
-		pass
+class Box:
+	type = "box"
 
+
+
+class Line:
+	type = "line"
+
+
+
+class ShapeDetector:
+	def __init__(self, source_file_path):
+		self.boxes = []
+		self.lines = []
+		self.image = cv2.imread(source_file_path)
+		pass
+		
+	def process(self):
+		# load the image and resize it to a smaller factor so that
+		# the shapes can be approximated better
+		image = self.image
+		resized = imutils.resize(image, width=300)
+		ratio = image.shape[0] / float(resized.shape[0])
+
+		# convert the resized image to grayscale, blur it slightly,
+		# and threshold it
+		gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+		blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+		# thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+		thresh = cv2.Canny(blurred, 50, 200)
+
+		# find contours in the thresholded image and initialize the
+		# shape detector
+
+		cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)
+		cnts = imutils.grab_contours(cnts)
+
+		# loop over the contours
+		for c in cnts:
+			# compute the center of the contour, then detect the name of the
+			# shape using only the contour
+			M = cv2.moments(c)
+			if M["m00"] == 0:
+				continue
+
+			cX = int((M["m10"] / M["m00"]) * ratio)
+			cY = int((M["m01"] / M["m00"]) * ratio)
+			shape = self.detect(c)
+
+			# multiply the contour (x, y)-coordinates by the resize ratio,
+			# then draw the contours and the name of the shape on the image
+			c = c.astype("float")
+			c *= ratio
+			c = c.astype("int")
+			cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+			cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+				0.5, (255, 0, 0), 2)
+				
+		self.processed_image = image
+		self.boxes = [r1, r2, r3]
+		self.lines = [l1, l2]
+		
 	def detect(self, c):
 		# initialize the shape name and approximate the contour
 		shape = "unidentified"
@@ -33,7 +92,9 @@ class ShapeDetector:
 
 		# otherwise, we assume the shape is a circle
 		else:
-			shape = "circle"
+			shape = "line"
+			object
+			self.objects.append()
 
 		# return the name of the shape
 		return shape
--- server/server.py
+++ server/server.py
@@ -61,48 +61,5 @@ def process(source_file_path):
 	sd.process()
 	return sd.processed_image, sd.boxes, sd.lines
 
-    # load the image and resize it to a smaller factor so that
-    # the shapes can be approximated better
-    image = cv2.imread(source_file_path)
-    resized = imutils.resize(image, width=300)
-    ratio = image.shape[0] / float(resized.shape[0])
-
-    # convert the resized image to grayscale, blur it slightly,
-    # and threshold it
-    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    # thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
-    thresh = cv2.Canny(blurred, 50, 200)
-
-    # find contours in the thresholded image and initialize the
-    # shape detector
-
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-    	cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-
-    # loop over the contours
-    for c in cnts:
-    	# compute the center of the contour, then detect the name of the
-    	# shape using only the contour
-    	M = cv2.moments(c)
-    	if M["m00"] == 0:
-    		continue
-
-    	cX = int((M["m10"] / M["m00"]) * ratio)
-    	cY = int((M["m01"] / M["m00"]) * ratio)
-    	shape = sd.detect(c)
-
-    	# multiply the contour (x, y)-coordinates by the resize ratio,
-    	# then draw the contours and the name of the shape on the image
-    	c = c.astype("float")
-    	c *= ratio
-    	c = c.astype("int")
-    	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-    	cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-    		0.5, (255, 0, 0), 2)
-
-    return image
-
 
 run(host='0.0.0.0', port=8080)
