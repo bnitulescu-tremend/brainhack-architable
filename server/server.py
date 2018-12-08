2e359028cfe418c1bcb4af6b948d1651d58be43c
Bogdan Nitulescu <bogdan.nitulescu@tremend.com>
2018-12-08 08:46:04 +0200

    dump

--- server/server.py
+++ server/server.py
@@ -81,7 +81,7 @@ class ArchResources:
             
 def send_arhitecture_request(text_json):
     if text_json is None:
-	return
+    return
     try:
         ar = ArchResources()
         
@@ -176,6 +176,10 @@ def rmfile(filename):
 def process(source_file_path, text):
     sd = ShapeDetector(source_file_path, text)
     sd.process()
+    for b in sd.boxes:
+        pprint(vars(b))
+    for l in sd.lines:
+        pprint(vars(l))
     return sd.processed_image, sd.boxes, sd.lines
 
 #send_arhitecture_request(json.loads('{"status":"Succeeded","recognitionResult":{"lines":[{"boundingBox":[284,250,367,264,360,306,277,292],"text":"HA","words":[{"boundingBox":[266,247,364,263,351,304,254,288],"text":"HA"}]},{"boundingBox":[111,510,192,498,203,574,122,585],"text":"Ell","words":[{"boundingBox":[87,487,218,507,222,578,91,558],"text":"Ell"}]},{"boundingBox":[296,521,398,532,394,570,292,559],"text":"We","words":[{"boundingBox":[327,530,375,530,373,565,325,565],"text":"We"}]},{"boundingBox":[733,522,834,526,833,561,732,558],"text":"FEB","words":[{"boundingBox":[712,517,846,527,844,565,710,555],"text":"FEB"}]},{"boundingBox":[389,764,513,778,509,814,385,800],"text":"ESB","words":[{"boundingBox":[383,769,482,773,482,806,382,802],"text":"ESB"}]},{"boundingBox":[89,900,206,900,198,900,81,900],"text":"DB","words":[{"boundingBox":[95,900,177,900,190,900,109,900],"text":"DB"}]},{"boundingBox":[653,900,807,900,806,900,652,900],"text":"DELASTIC","words":[{"boundingBox":[636,900,808,900,809,900,637,900],"text":"DELASTIC"}]}]}}'))
